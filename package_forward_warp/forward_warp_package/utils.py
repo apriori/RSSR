e80ff5ed914d7a579549472a2e5592a6385bd349
Armin Kazmi <armin.kazmi@smartoptics.de>
2023-05-08 18:54:54 +0200

    Try to get everything working with just 100 faces

--- package_forward_warp/forward_warp_package/utils.py
+++ package_forward_warp/forward_warp_package/utils.py
@@ -1,12 +1,28 @@
 import torch
 import meshzoo
+import numpy as np
+import math
 
 def generate_2D_mesh(H, W):
-    _, faces = meshzoo.rectangle(
-        xmin = -1., xmax = 1.,
-        ymin = -1., ymax = 1.,
-        nx = W, ny = H,
-        zigzag=True)
+    # try to circumvent meshzoo license limit
+    aspect = H/W
+
+    if aspect > 1.0:
+       hh = 10
+       ww = 10/aspect
+    else:
+       ww = 10
+       hh = 10*aspect
+
+    print(f"H: {hh}")
+    print(f"W: {ww}")
+    W = int(ww)
+    H = int(hh)
+
+    _, faces = meshzoo.rectangle_tri(
+       np.linspace(-1.0, 1.0, int(ww)),
+       np.linspace(-1.0, 1.0, int(hh)),
+       variant='zigzag')
 
     x = torch.arange(0, W, 1).float().cuda() 
     y = torch.arange(0, H, 1).float().cuda()
